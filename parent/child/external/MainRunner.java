6415b7d5c111ff7b581406c5e7143a49d0ee7300
Kantharaj <kantharajbhandary@gmail.com>
2025-04-02 20:08:37 +0530

    inheritance

--- parent/child/external/MainRunner.java
+++ parent/child/external/MainRunner.java
@@ -1,9 +1,11 @@
 package parent.child.external;
 
+import parent.child.internal.Cloth;
 import parent.child.internal.Cricket;
+import parent.child.internal.Jeans;
 import parent.child.internal.TestMatch;
 
-public class TestMatchRunner {
+public class MainRunner {
     public static void main(String[] args) {
         Cricket cricket=new Cricket();
         cricket.matchLocation();
@@ -19,7 +21,6 @@ public class TestMatchRunner {
         testMatch.totalRuns();
         testMatch.totalWickets();
         System.out.println("===================");
-
         Cricket cricket1=new TestMatch();
         cricket1.matchLocation();
         cricket1.matchTime();
@@ -27,5 +28,27 @@ public class TestMatchRunner {
         cricket1.totalRuns();
         cricket1.totalWickets();
         System.out.println("===================");
+
+
+        Cloth cloth=new Cloth();
+        cloth.cost();
+        cloth.durability();
+        cloth.material();
+        cloth.warrenty();
+        cloth.size();
+        System.out.println("===================");
+        Jeans jeans=new Jeans();
+        jeans.cost();;
+        jeans.durability();
+        jeans.material();
+        jeans.size();
+        jeans.warrenty();
+        System.out.println("===================");
+        Cloth cloth1=new Jeans();
+        cloth1.size();
+        cloth1.material();
+        cloth1.warrenty();
+        cloth1.cost();
+        cloth1.warrenty();
     }
 }
---
+++ parent/child/internal/Jeans.java
@@ -0,0 +1,9 @@
+package parent.child.internal;
+
+public class Jeans extends Cloth{
+    public Jeans()
+    {
+        System.out.println("Running no args const in jeans");
+    }
+
+}
