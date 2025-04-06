f94a9e592067406a1ea94b48bb7f28dad7aa7f30
Kantharaj <kantharajbhandary@gmail.com>
2025-04-06 23:49:29 +0530

    inheritance updated

--- parent/child/internal/Genetics.java
+++ parent/child/internal/Genetics.java
@@ -4,4 +4,25 @@ public class Genetics extends Biology {
     public Genetics() {
         System.out.println("Running no-arg constructor in Genetics");
     }
+
+    @Override
+    public void study() {
+        System.out.println("Biology is the study of living organisms...");
+    }
+
+    public void branches() {
+        System.out.println("it includes zoology, botany, genetics, and microbiology.");
+    }
+
+    public void evolution() {
+        System.out.println("it Explains how life has evolved over time.");
+    }
+
+    public void humanBody() {
+        System.out.println("the Biology helps in understanding human anatomy and physiology.");
+    }
+
+    public void medicalImpact() {
+        System.out.println("the Biology contributes to medicine and healthcare.");
+    }
 }
