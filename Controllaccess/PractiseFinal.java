692a41cb9a6d3f70e152d6bffab7d4d4c1a9c065
Kantharaj <kantharajbhandary@gmail.com>
2025-04-14 21:33:56 +0530

    final

---
+++ Controllaccess/PractiseFinal.java
@@ -0,0 +1,11 @@
+package Controllaccess;
+
+public class PractiseFinal {
+    private final String name="kantharaj";
+    private final int age=21;
+    static {
+        name="kbm";
+        age="22";//we cannot reinitialize
+    }
+
+}
